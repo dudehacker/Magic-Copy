7f188aa60d29b14d52c701fd0fa4ea03cfc9632f
Quansheng.Xiong <Quansheng.Xiong@fdmgroup.com>
2019-02-08 11:07:44 -0500

    refactor, not tested

--- src/main/MagicCopyMania.java
+++ src/main/MagicCopyMania.java
@@ -1,39 +1,24 @@
 package main;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 import javax.swing.JOptionPane;
 
 import osu.beatmap.Beatmap;
-import osu.beatmap.BeatmapUtils;
-import osu.beatmap.Chord;
-import osu.beatmap.event.Sample;
-import osu.beatmap.hitobject.HitObject;
-import osu.beatmap.timing.Timing;
 
-public class MagicCopyMania implements Runnable{
-	
-	//Variables
+public class MagicCopyMania implements Runnable {
+
+	// Variables
 	private File targetFile;
 	private Beatmap sourceBeatmap;
 	private Beatmap targetBeatmap;
 	private boolean isKeysound;
 	private boolean clearHS;
-	private List<HitObject> List_SourceHS = new ArrayList<>();
-	private List<HitObject> List_TargetHS = new ArrayList<>();
-	private List<Sample> List_Samples = new ArrayList<>();
-	private List<Timing> sourceTimingTotal;
-	private List<Timing> targetTiming;
-	private String nl = BeatmapUtils.nl;
-	
+
 	// Constructor
-	public MagicCopyMania(File input, File output, boolean keysound, boolean clear) throws ParseException, IOException{
+	public MagicCopyMania(File input, File output, boolean keysound, boolean clear) throws ParseException, IOException {
 		sourceBeatmap = new Beatmap(input);
 		targetFile = output;
 		targetBeatmap = new Beatmap(output);
@@ -43,305 +28,16 @@ public class MagicCopyMania implements Runnable{
 
 	@Override
 	public void run() {
+		if (clearHS) {
+			sourceBeatmap.clearHitsounds();
+		}
+		targetBeatmap.copyHS(sourceBeatmap, isKeysound);
 		try {
-			if (clearHS){
-				sourceBeatmap.clearHitsounds();
-			}
-			parseSource();
-			parseTarget();
-			exportBeatmap();
-			JOptionPane.showMessageDialog(null, "Beatmap exported at " + targetFile.getAbsolutePath());
-		} catch (Exception e) {
+			targetBeatmap.exportBeatmap(targetFile);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		JOptionPane.showMessageDialog(null, "Beatmap exported at " + targetFile.getAbsolutePath());
 	}
-	
-	private void parseSource() throws Exception{
-		List_SourceHS = sourceBeatmap.getHitObjectSection().getHitObjects();
-		System.out.println("Hitsound total size = "+List_SourceHS.size());
-		sourceTimingTotal = sourceBeatmap.getTimingSection().getTimingPoints();
-		List_Samples = sourceBeatmap.getEventSection().getSamples();
-
-	}
-	
-	private void parseTarget() throws Exception{
-		List_TargetHS = targetBeatmap.getHitObjectSection().getHitObjects();
-		targetTiming = targetBeatmap.getTimingSection().getTimingPoints();
-	}
-	
-	
-	private void copyHS() {
-		Set<Chord> sourceChords = sourceBeatmap.getChords();
-		Set<Chord> targetChords = targetBeatmap.getChords();
-		
-		if (isKeysound) {
-			targetChords.removeIf(entry -> {
-				for (Chord chord : sourceChords) {
-					if (chord.getStartTime() == entry.getStartTime()) {
-						return true;
-					}
-					
-				}
-				return false;
-			});
-			targetBeatmap.copyHS(targetChords);
-		}
-		
-		List<Long> list_time = BeatmapUtils.getDistinctStartTime(List_SourceHS,List_TargetHS);
-		for (Long t : list_time){
-			List<HitObject> sourceChord = BeatmapUtils.getChordByTime(List_SourceHS, t);
-			List<HitObject> targetChord = BeatmapUtils.getChordByTime(List_TargetHS, t);
-		}
-	}
-	
-	
-	/*
-	private void copyHS(){
-		
-		
-		List<Long> list_time = BeatmapUtils.getDistinctStartTime(List_SourceHS,List_TargetHS);
-		for (Long t : list_time){
-			
-			List<HitObject> sourceChord = BeatmapUtils.getChordByTime(List_SourceHS, t);
-			List<HitObject> targetChord = BeatmapUtils.getChordByTime(List_TargetHS, t);
-			
-			int sourceSize = sourceChord.size();
-			int targetSize = targetChord.size();
-			
-			if (sourceSize == targetSize){
-				// CASE 1
-				//System.out.println("same size at " +t);
-				for (int i = 0; i<targetSize;i++){
-					HitObject source_ho = sourceChord.get(i);
-					HitObject target_ho = targetChord.get(i);
-					target_ho.copyHS(source_ho);
-				}
-			} else if (sourceSize> targetSize){
-				// CASE 2
-				System.out.println("sourceSize > targetSize at " +t);
-				if (targetSize ==0 ){
-					if (isKeysound){
-						// keysound = true then copy to SB, else do nothing
-						for (int j = 0; j<sourceSize;j++){
-							HitObject source_ho = sourceChord.get(j);
-							if (source_ho.toSample().toString().contains(".wav")){
-								List_Samples.addAll(source_ho.toSample());
-							}
-						}
-						
-					}
-
-				}  else{
-					int defaultHitSoundSize = BeatmapUtils.getDefaultHSChordSizeForTime(sourceChord, t);
-					switch (defaultHitSoundSize){
-					case 0:
-					case 1:
-						System.out.println("source size 0|1 at " +t);
-						for (int i = 0; i<targetSize;i++){
-							HitObject source_ho = sourceChord.get(i);
-							HitObject target_ho = targetChord.get(i);
-							target_ho.copyHS(source_ho);
-						}
-						for (int j = targetSize; j < sourceSize; j++){
-							HitObject source_ho = sourceChord.get(j);
-							if (source_ho.toSample().toString().contains(".wav")){
-								List_Samples.addAll(source_ho.toSample());
-							}
-						}
-						break;
-					
-						
-					case 2: // Combine both default hitsounds into 1 HitObject
-						System.out.println("source size 2 at " +t);
-						outputHOs.addAll(combineDefaultHS(sourceChord,targetChord,2));
-						break;
-						
-					case 3:
-						System.out.println("source size 3 at " +t);
-//						System.out.println("target size " + targetSize);
-						if (targetSize >= 2){
-							outputHOs.addAll(combineDefaultHS(sourceChord,targetChord,2));
-						} else {
-							outputHOs.addAll(combineDefaultHS(sourceChord,targetChord,3));
-						}
-						break;
-					}
-				}
-			
-			}else{
-				// CASE 3 sourceSize < targetSize
-				//System.out.println("sourceSize < targetSize at " +t);
-				for (int i = 0; i<sourceSize;i++){
-					HitObject source_ho = sourceChord.get(i);
-					HitObject target_ho = targetChord.get(i);
-					target_ho.copyHS(source_ho);
-					outputHOs.add(target_ho);
-				}
-				for (int j = sourceSize; j<targetSize;j++){
-					HitObject target_ho = targetChord.get(j);
-					outputHOs.add(target_ho);
-				}
-			}
-		}
-		Collections.sort(outputHOs, HitObject.StartTimeComparator);
-		for (HitObject ho : outputHOs) {
-			output += ho.toString() + nl;
-		}
-	}
-	
-
-	
-	private ArrayList<HitObject> combineDefaultHS(ArrayList<HitObject> sourceChord, ArrayList<HitObject> targetChord, int n){
-		
-		ArrayList<HitObject> output = new ArrayList<>();
-		Collections.sort(sourceChord, HitObject.AdditionComparator);
-		int sourceSize = sourceChord.size();
-		int targetSize = targetChord.size();
-		HitObject source_ho1 = sourceChord.get(0);
-		HitObject source_ho2 = sourceChord.get(1);
-		HitObject newHO = source_ho1.clone();
-		if (n==2){
-			if (newHO.getAddition() == source_ho2.getAddition()) {
-				newHO.addWhistleFinishClap(source_ho2.getWhistleFinishClap());
-				
-				HitObject target_ho1 = targetChord.get(0);
-				target_ho1.copyHS(newHO);
-				output.add( target_ho1);
-				for (int x = 0; x < n ; x++){
-					sourceChord.remove(0);
-				}
-				targetChord.remove(0);
-				
-			} 
-			
-		} else if (n==3){
-			HitObject source_ho3 = sourceChord.get(2);
-			if (source_ho3.getAddition() == source_ho2.getAddition() && source_ho2.getAddition() == source_ho1.getAddition()) {
-				newHO.addWhistleFinishClap(source_ho2.getWhistleFinishClap(),source_ho3.getWhistleFinishClap());
-				
-				HitObject target_ho1 = targetChord.get(0);
-				target_ho1.copyHS(newHO);
-				output.add( target_ho1);
-				for (int x = 0; x < n ; x++){
-					sourceChord.remove(0);
-				}
-				targetChord.remove(0);
-				
-			} else if (source_ho1.getAddition()==source_ho2.getAddition()) {
-				newHO.addWhistleFinishClap(source_ho2.getWhistleFinishClap());
-				
-				HitObject target_ho1 = targetChord.get(0);
-				target_ho1.copyHS(newHO);
-				output.add( target_ho1);
-				for (int x = 0; x < 2 ; x++){
-					sourceChord.remove(0);
-				}
-				targetChord.remove(0);
-				
-			} else if(source_ho2.getAddition() == source_ho3.getAddition()) {
-				newHO = source_ho2.clone();
-				newHO.addWhistleFinishClap(source_ho3.getWhistleFinishClap());
-				
-				HitObject target_ho1 = targetChord.get(0);
-				target_ho1.copyHS(newHO);
-				output.add( target_ho1);
-				sourceChord.remove(1);
-				sourceChord.remove(1);
-				targetChord.remove(0);
-			} 
-			
-		} else {
-			throw new IllegalArgumentException();
-		}
-
-		// copy rest of hitsounds
-		try {
-		if (sourceChord.size()>0 && targetChord.size()>=0){
-			for (int i = 0; i<targetChord.size();i++){
-				HitObject source_ho = sourceChord.get(i);
-				HitObject target_ho = targetChord.get(i);
-				target_ho.copyHS(source_ho);
-				output.add(target_ho);
-			}
-			for (int j = targetChord.size(); j < sourceChord.size(); j++){
-				HitObject source_ho = sourceChord.get(j);
-				List_Samples.add(source_ho.toSample());
-			}
-			
-		}
-		
-		} catch (Exception e){
-			System.out.println(n + " targetsize " + targetSize + " source size " + sourceSize);
-			e.printStackTrace();
-		}
-		return output;
-	}
-*/
-	
-	@SuppressWarnings("unchecked")
-	private void exportBeatmap() throws Exception{
-		copyHS();
-		// only copy useful timing for default hitsounds to target difficulty
-
-		Timing t1, t2;
-		for (Timing t : targetTiming){
-			for (int i = 0; i < sourceTimingTotal.size(); i++){
-				if (i<sourceTimingTotal.size()-1){
-					t1 = sourceTimingTotal.get(i);
-					t2 = sourceTimingTotal.get(i+1);
-					if (t1.getOffset() <= t.getOffset() && t.getOffset() < t2.getOffset()){
-						copyTimingHS(t1,t);
-						break;
-					}
-				} else {
-					// last timing
-					copyTimingHS(sourceTimingTotal.get(i),t);
-				}
-			}
-		}
-		// add timings that exist in HS but not in target
-		ArrayList<Timing> targetTimingCopy = (ArrayList<Timing>) targetTiming.clone();
-		ArrayList<Long> offsets = new ArrayList<>();
-		
-		for (Timing t_target: targetTimingCopy){
-			if (!offsets.contains(t_target.getOffset())){
-				offsets.add(t_target.getOffset());
-			}
-		}
-		for (Timing t_source : sourceTimingTotal){
-			if (!offsets.contains(t_source.getOffset())){
-				Timing t = getTimingFromOffset(t_source.getOffset());
-				targetTiming.add(t);
-			}
-		}
-		/*System.out.println("Size of target Timings = " +targetTiming.size());
-		for (Timing t : targetTiming) {
-			System.out.println(t.toString());
-		}*/
-		targetTiming.sort(Timing.StartTimeComparator);
-		BeatmapUtils.exportBeatmap(targetFile, targetBeatmap.toString());
-	}
-	
-	private Timing getTimingFromOffset(long offset){
-		
-		for (Timing t:sourceTimingTotal){
-			//System.out.println(t.toString());
-			if (t.getOffset() == offset){
-				return t;
-			}
-		}
-		System.out.println("offset = "+offset);
-		System.out.println("Error in getTimingFromOffset()");
-		return null;
-	}
-	
-	private void copyTimingHS(Timing source, Timing target){
-		target.setSetID(source.getSetID());
-		target.setVolume(source.getVolume());
-		target.setSampleSet(source.getSampleSet());
-	}
-	
-
 
 }
